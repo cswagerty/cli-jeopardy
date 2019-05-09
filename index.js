b95326be27af903691a05a3c905047c31bb8d749
Corbin Swagerty <corbin.swagerty@gmail.com>
2019-05-09 15:39:02 -0700

    parse task now creates categories and clues data structures to be output into a json file

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const cheerio = require('cheerio');
 const fs = require('fs');
 
-fs.readFile('./j-archive-data/season-35/7816.html', (err, buf) => {
-	const gameHtml = buf.toString();
-	const categories = getCategories(gameHtml);
-	const clues = getClues(gameHtml);
-});
+function parseEpisodeHtml(episodeId) {
+	// parse episode's HTML file from J!Archive and reformat into JSON data
+	let boards = [];  // there will be two boards, for jeopardy and double jeopardy
+	let jeopardyBoard = {
+		roundName: 'jeopardy',
+		categories: [],
+		clues: []
+	};
+
+	fs.readFile(`./j-archive-data/season-35/${episodeId}.html`, (err, buf) => {
+		const gameHtml = buf.toString();
+		let categories = getCategories(gameHtml);
+		// only use first 6 categories in jeopardy round
+		categories.splice(categories.length / 2); 
+		jeopardyBoard.categories = categories;
+		// only add the first 6 categories
+		let clues = getClues(gameHtml);
+		jeopardyBoard.clues = getRoundClues(clues);
+	});
+}
 
 function getCategories(html) {
 	const $ = cheerio.load(html);
@@ -15,7 +30,10 @@ function getCategories(html) {
 	let categories = [];
 
 	categoryEls.each((i, el) => {
-		categories[i] = $(el).text();
+		categories[i] = {
+			name: $(el).text(),
+			id: i
+		};
 	});
 	return categories;
 }
@@ -28,11 +46,68 @@ function getClues(html) {
 	let clues = [];
 
 	clueEls.each((i, el) => {
-		let str = $(el).attr('onmouseout');
-		// strip out inline js and escaped single quotes used only on website
-		let question = str.replace(/^.*stuck',\s'/, '').replace(/'\)/, '').replace(/\\/g, '');
-		// remove html tags
-		return cheerio.load(question).text();
+		$el = $(el);
+
+		clues[i] = {
+			clueText: getClueText($el),
+			correctResponse: getClueCorrectResponse($el)
+		};
 	});
 	return clues;
 }
+
+function getClueText($el) {
+	let str = $el.attr('onmouseout');
+	// strip out inline js and escaped single quotes used only on website
+	let question = str.replace(/^.*stuck',\s'/, '').replace(/'\)/, '').replace(/\\/g, '');
+	// remove html tags
+	return cheerio.load(question).text();
+}
+
+function getClueCorrectResponse($el) {
+	let str = $el.attr('onmouseover');
+
+
+	// strip out inline js and escaped single quotes used only on website
+	let responseEls = str.replace(/^.*stuck',\s'/, '').replace(/'\)$/, '');
+	const $ = cheerio.load(responseEls)
+	return $('.correct_response').text();
+}
+
+function getRoundClues(clues) {
+	// create array of clue objects with categoryIds and value amounts
+	let roundClues = [ ...clues ];
+	roundClues.splice(30);
+
+	// assign a dollar value and categoryId to each clue
+
+	return roundClues.map((clue, index) => {
+		return {
+			...clue,
+			categoryId: getClueCategoryId(index),
+			value: getClueValue(index)
+		}
+	});
+}
+
+function getClueCategoryId(index) {
+	// index 0 -> 0
+	// index 1 -> 1
+	// index 6 -> 0
+	// index 7 -> 1
+
+	const NUMBER_OF_CATEGORIES = 6;
+	return index % NUMBER_OF_CATEGORIES;
+}
+
+function getClueValue(index) {
+	// index 0-5 -> $100
+	// index 6-10 -> $200
+	// etc.
+
+	const NUMBER_OF_COLUMNS = 6;
+	const clueColumn = Math.floor(index / NUMBER_OF_COLUMNS) + 1;
+	return clueColumn * 100;
+}
+
+parseEpisodeHtml(7816);
